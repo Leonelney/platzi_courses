9b105ce51936f8a78b924f6783d9d98abc22773a
leonelOC <leonelolivares80@gmail.com>
2022-11-30 20:58:02 -0600

    iniciamos el curso de fundamentos de R

---
+++ fundamentos_R/01_variables.R
@@ -0,0 +1,14 @@
+# podemos haces operaciones sin la necesidad de usar una función para imprimir el
+# resultado
+4 + 8
+20 - 8
+4^2
+# para crear una variable ahora no usamos el igual, usamos el <-
+x <- 86
+x
+# ejemplo de uso de variables
+oficina <- 7
+platzi <- 1
+transporte <- 1.5
+tiempo_al_dia <- oficina + platzi + transporte
+tiempo_al_dia
